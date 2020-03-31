75a5ee3af1db0f0d24a8c1f20aa94970848ebdf7
sunliang <sunliang711@163.com>
2020-03-31 10:41:30 +0800

    not source shell-header.sh; TODO: install this tool to PATH

--- install.sh
+++ install.sh
@@ -5,12 +5,7 @@ if [ -z "$rpath" ];then
 fi
 root="$(cd $(dirname $rpath) && pwd)"
 cd "$root"
-shellHeaderLink='https://pic711.oss-cn-shanghai.aliyuncs.com/sh/shell-header.sh'
-if [ -e /etc/shell-header.sh ];then
-    source /etc/shell-header.sh
-else
-    (cd /tmp && wget -q "$shellHeaderLink") && source /tmp/shell-header.sh
-fi
+
 # write your code below
 usage(){
     cat<<-EOF
